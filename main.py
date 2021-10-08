01cb50bb7465f2d965079834a6f181da429063c2
Philip Cammarata <philip@cammarata.me>
2021-10-07 20:16:13 -0400

    creature queue is done

--- main.py
+++ main.py
@@ -51,18 +51,29 @@ class Game(arcade.Window):
 
         for index, creature in enumerate(self.creature_list):
             creature.change_x = -CREATURE_SPEED
-            if arcade.check_for_collision_with_lists(
-                creature, [self.hero_list, self.creature_list]
-            ):
+
+            if arcade.check_for_collision_with_list(creature, self.hero_list):
                 creature.change_x = 0
-            if (
-                arcade.check_for_collision_with_list(creature, self.hero_list)
-                and self.attack_timer > 1
-            ):
+
+            creature_index = self.creature_list.index(creature)
+            if creature_index >= 1:
+                if arcade.check_for_collision(creature, self.creature_list[creature_index - 1]):
+                    creature.change_x = 0
+
+            if arcade.check_for_collision_with_list(creature, self.hero_list) and self.attack_timer > 1:
                 self.attack_timer = 0
                 creature.health -= 1
                 if creature.health <= 0:
                     creature.remove_from_sprite_lists()
+            """
+            if arcade.check_for_collision_with_lists(creature, [self.hero_list, self.creature_list]):
+                creature.change_x = 0
+            if arcade.check_for_collision_with_list(creature, self.hero_list) and self.attack_timer > 1:
+                self.attack_timer = 0
+                creature.health -= 1
+                if creature.health <= 0:
+                    creature.remove_from_sprite_lists()
+            """
 
     def add_creature(self, delta_time):
         if len(self.creature_list) < 7:
